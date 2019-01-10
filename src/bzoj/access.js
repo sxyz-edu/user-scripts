c678bc989a6e19898e55720799255e7de0814d35
sxyugao <sxyugao@qq.com>
2019-01-10 16:12:12 +0800

    minor enhancement

--- src/bzoj/access.js
+++ src/bzoj/access.js
@@ -2,6 +2,13 @@
  * Provide access to protected problems
  */
 
-if (location.href.indexOf('problem.php') > -1) {
+if (document.title === 'Please contact lydsy2012@163.com!' && location.href.indexOf('problem.php') > -1) {
   location.href = location.href.replace('problem.php', 'show.php');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (location.href.indexOf('show.php') > -1) {
+    const ele = document.getElementsByTagName('h2')[0];
+    ele.innerHTML += '<span style="color:red;">[权限题]</span>';
+  }
+})
