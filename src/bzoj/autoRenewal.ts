a9f4fbd83aee5d649eb6e310341ff31f50ff4c23
sxyugao <sxyugao@qq.com>
2019-07-18 11:13:45 +0800

    some optimization

--- src/bzoj/autoRenewal.ts
+++ src/bzoj/autoRenewal.ts
@@ -1,11 +1,11 @@
 /**
  * Keep your session
  *
- * Just need to visit homepage every 10 minutes
+ * Just need to visit homepage every minute
  */
 
 setInterval(() => {
   fetch("/JudgeOnline/", {
     credentials: "same-origin",
   });
-}, 1000 * 60 * 10);
+}, 1000 * 60 * 1);
