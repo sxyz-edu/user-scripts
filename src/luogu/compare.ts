a1eacb74355dccdbc1e7f6c5c239608790d32a69
sxyugao <sxyugao@qq.com>
2019-11-07 19:29:24 +0800

    adapt luogu4.0

--- src/luogu/compare.ts
+++ src/luogu/compare.ts
@@ -6,19 +6,6 @@
  * - Other: red
  */
 
-const dataRegex = /<div class="lg-article am-hide-sm">([\s\S]*?)<\/div>/gi;
-const spanRegex = /<span[\s\S]*?<\/span>/gi;
-const aRegex = /<a[^>]*?>([\s\S]*?)<\/a>/gi;
-
-/**
- * Parse problem id from html source text
- * @param {String} str html source text
- * @returns {[String]} problem ids
- */
-const parseProblems = (str: string): string[] => {
-  return (str.match(aRegex) || []).map((res) => res.replace(aRegex, '$1'));
-};
-
 /**
  * ProblemList for caching
  */
@@ -29,7 +16,7 @@ interface IProblemList {
 }
 
 /**
- * Get list of problems who passed or tried
+ * Get list of problems user passed or tried
  * @param {String} uid user id
  * @return {Promise<ProblemList>} problem list
  */
@@ -47,20 +34,16 @@ const getProblemList = (uid: string): Promise<IProblemList> => {
       }
     }
 
-    const parseResult = (result: string) => {
-      if (!result) {
-        reject(new Error('Parse error'));
-      }
-
-      return result;
-    };
-
-    fetch(`/space/show?uid=${uid}`)
+    fetch(`/user/${uid}#problem`)
       .then((res) => res.text())
       .then((res) => {
-        const data = (res.match(dataRegex) || []).map((match) => match.replace(spanRegex, ''));
-        const passedlist = parseProblems(parseResult(data[1]));
-        const triedlist = parseProblems(parseResult(data[2]));
+        const s = (/decodeURIComponent\("(.*\..*)"\)/).exec(res);
+        if (!s) {
+          return;
+        }
+        const data = JSON.parse(decodeURIComponent(s[1]));
+        const passedlist = data.currentData.passedProblems;
+        const triedlist = data.currentData.submittedProblems;
         const save = {
           passedlist,
           triedlist,
@@ -74,67 +57,89 @@ const getProblemList = (uid: string): Promise<IProblemList> => {
 };
 
 /**
- * Display a number on a h2 element
- * @param {Number} num number need to be show
- * @returns {void} nothing
+ * Generator problem set
+ * @param {Array<any>} list the input
+ * @return {Set} the output
  */
-const displayNumber = (num: number): void => {
-  const el = document.querySelector('span.fuck-spider') as HTMLElement;
-  const h2 = el.parentElement as HTMLElement;
-  h2.style.fontSize = '18px';
-  h2.textContent = `通过题目（其中你有 ${num} 道题尚未 AC）`;
-};
+const gen = (list: Array<any>): Set<any> => {
+  const res = new Set();
+  list.forEach((pro) => {
+    res.add(pro.pid);
+  });
+
+  return res;
+}
 
 export default () => {
-  if (window.location.pathname === '/space/show') {
-    const anotherMatch = (/uid=(\d+)/).exec(window.location.href);
-    if (!anotherMatch) {
-      // if this is not a profile page
-      return;
-    }
-    const another = anotherMatch[1];
-
-    const waitForLoaded = (): void => {
-      const el = document.querySelector('img.avatar');
-      if (!el) {
-        setTimeout(waitForLoaded, 100);
-      } else {
-        const src = el.getAttribute('src') as string;
-        const uidMatch = (/usericon\/(\d+)/i).exec(src);
-        if (!uidMatch) {
-          return;
-        }
-        const uid = uidMatch[1];
-        if (uid === '1') {
-          // the current user must be logged in
-          return;
-        }
-        if (another === uid) {
-          // compared with the current user logged in
-          return;
-        }
-        getProblemList(uid).then((data) => {
-          let num = 0;
-          const passedlist = new Set(data.passedlist);
-          const triedlist = new Set(data.triedlist);
-          const list = Array.from(document.querySelectorAll('div.lg-article > a[data-pjax]'));
-          list.forEach((a) => {
-            const pid = a.innerHTML;
-            if (passedlist.has(pid)) {
-              a.classList.add('solved');
-            } else {
-              ++num;
-              if (triedlist.has(pid)) {
-                a.classList.add('tried');
-              } else {
-                a.classList.add('unsolved');
-              }
-            }
-          });
-          displayNumber(num);
-        });
+  const waitForAvatarLoaded = (): void => {
+    const el = document.querySelector('img.avatar');
+    if (!el) {
+      setTimeout(waitForAvatarLoaded, 100);
+    } else {
+      const avatar = document.querySelector('img.avatar') as HTMLElement;
+      const src = avatar.getAttribute('src') as string;
+      const uidMatch = (/usericon\/(\d+)/i).exec(src);
+      if (!uidMatch) {
+        // it's impossible
+        return;
       }
-    };
-    waitForLoaded();
+      const uid = uidMatch[1];
+      if (uid === '1') {
+        // the current user must be logged in
+        return;
+      }
+      let num = 0;
+      const watchDog = (): void => {
+        if (!window.location.href.includes('#problem')) {
+          // if not in problem page
+          num = 0;
+          setTimeout(watchDog, 1000);
+        } else if (!num) {
+          // only run one time
+          ++num;
+          const anotherMatch = (/(\d+)#problem/).exec(window.location.href);
+          if (!anotherMatch) {
+            // it's impossible
+            return;
+          }
+          const another = anotherMatch[1];
+          if (another === uid) {
+            // compared with the current user logged in
+            setTimeout(watchDog, 1000);
+          } else {
+            getProblemList(uid).then((data) => {
+              const passedlist = gen(Array.from(data.passedlist));
+              const triedlist = gen(Array.from(data.triedlist));
+              const waitForLoaded = (): void => {
+                const e = document.querySelector('div.row');
+                if (e) {
+                  const list = Array.from(document.querySelectorAll('span.problem-id'));
+                  list.forEach((a) => {
+                    const el = a.firstElementChild;
+                    if (el) {
+                      const ele = el as HTMLElement;
+                      const pid = ele.innerHTML;
+                      if (passedlist.has(pid)) {
+                        ele.classList.add('solved');
+                      } else if (triedlist.has(pid)) {
+                        ele.classList.add('tried');
+                      } else {
+                        ele.classList.add('unsolved');
+                      }
+                    }
+                  });
+                } else {
+                  setTimeout(waitForLoaded, 500);
+                }
+              }
+              waitForLoaded();
+            });
+          }
+        }
+        setTimeout(watchDog, 1000);
+      }
+      watchDog();
+    }
   }
+  waitForAvatarLoaded();
 };
